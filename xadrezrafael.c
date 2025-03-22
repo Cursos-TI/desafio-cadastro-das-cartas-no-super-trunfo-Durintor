caac4aa8ae5ac2c9c6b32a73abe6b02a8e113936
Durintor <rhss1822@gmail.com>
2025-03-22 15:12:47 +0000

    inicio

---
+++ xadrezrafael.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+int main(){
+
+   
+
+    char estado = 'R';
+    char cidade [50];
+    int habitantes;
+
+    printf("Desafio Xadrez!\n");
+    printf("N° de habitantes: %s");
+    scanf("%d", &habitantes);
+    printf("Habitantes: %d", habitantes);
+
+
+
+return 0;
+
+
+
+
+}
